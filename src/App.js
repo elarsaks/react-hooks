f02c154c431cea1097f5ab4c4431a47126ac54f5
Elar Saks <elarsaks@gmail.com>
2023-08-02 02:08:22 +0300

    Create dummy components.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import fs from "fs";
+import logo from "./logo.svg";
+import path from "path";
+import React, { Component } from "react";
+
 
 class App extends Component {
   render() {
+    const componentFiles = fs.readdirSync(path.join(__dirname, "components"));
+
     return (
       <div className="App">
         <div className="App-header">
@@ -13,6 +19,16 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <Switch>
+          {componentFiles.map((file) => {
+            const component = require(`./components/${file}`).default;
+            const routePath = "/" + path.basename(file, ".js").toLowerCase();
+            return (
+              <Route key={routePath} path={routePath} component={component} />
+            );
+          })}
+        </Switch>
       </div>
     );
   }
