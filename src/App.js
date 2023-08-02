cae25d968018adffff3cae137d6afc54d6683500
Elar Saks <elarsaks@gmail.com>
2023-08-02 03:21:27 +0300

    Create router header.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import "./App.css";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link,
-  useHistory,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 
 import UseCallback from "./pages/UseCallback";
 import UseContext from "./pages/UseContext";
@@ -17,15 +11,37 @@ import PageNotFound from "./pages/PageNotFound";
 import UseState from "./pages/UseState";
 import logo from "./logo.svg";
 
+function Header() {
+  const navigate = useNavigate();
+
+  const handleRouteChange = (event) => {
+    navigate(event.target.value);
+  };
+
+  return (
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>React Hooks</h1>
+
+      <select onChange={handleRouteChange}>
+        <option value="/">Home</option>
+        <option value="/useState">UseState</option>
+        <option value="/useCallback">UseCallback</option>
+        <option value="/useContext">UseContext</option>
+        <option value="/useEffect">UseEffect</option>
+        <option value="/useMemo">UseMemo</option>
+        <option value="/useReducer">UseReducer</option>
+        <option value="/useRef">UseRef</option>
+      </select>
+    </div>
+  );
+}
+
 export default function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>React Hooks</h1>
-      </div>
-
-      <BrowserRouter>
+    <BrowserRouter>
+      <div className="App">
+        <Header /> {/* Now, Header is inside BrowserRouter */}
         <Routes>
           <Route path="/">
             {/* UseState Route */}
@@ -54,7 +70,7 @@ export default function App() {
             <Route path="*" element={<PageNotFound />} />
           </Route>
         </Routes>
-      </BrowserRouter>
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
