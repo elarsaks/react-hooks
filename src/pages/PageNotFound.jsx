c2c8c61b82515c8d0bdbc57b7889ed1d0fafb87f
Elar Saks <elarsaks@gmail.com>
2023-08-03 01:32:50 +0300

    Update 404 page.

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 
 const NotFound = () => {
+  const containerStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+
+    color: "#333",
+  };
+
+  const headingStyle = {
+    fontSize: "48px",
+    fontWeight: "bold",
+    marginBottom: "20px",
+    color: "#FFFFFF",
+  };
+
+  const paragraphStyle = {
+    fontSize: "24px",
+    textAlign: "center",
+    color: "#FFFFFF",
+  };
+
   return (
-    <div>
-      <h1>404 - Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
+    <div style={containerStyle}>
+      <h1 style={headingStyle}>404 - Not Found</h1>
+      <p style={paragraphStyle}>The page you are looking for does not exist.</p>
     </div>
   );
 };
