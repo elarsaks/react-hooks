5c9987513aceae33d5466a59c6cb8a0a0f63cd38
Elar Saks <elarsaks@gmail.com>
2023-08-03 00:19:00 +0300

    Fix navigation on load.

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,20 +1,31 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import logo from "../logo.svg";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function AppHeader() {
   const navigate = useNavigate();
+  const location = useLocation();
+  const [selectedOption, setSelectedOption] = useState(location.pathname);
 
   const handleRouteChange = (event) => {
     navigate(event.target.value);
   };
+
+  // Update the selected option whenever the location changes
+  useEffect(() => {
+    setSelectedOption(location.pathname);
+  }, [location]);
 
   return (
     <div className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <h1>React Hooks</h1>
 
-      <select className="header-selector" onChange={handleRouteChange}>
+      <select
+        className="header-selector"
+        onChange={handleRouteChange}
+        value={selectedOption}
+      >
         <option value="/useState">UseState</option>
         <option value="/useCallback">UseCallback</option>
         <option value="/useContext">UseContext</option>
