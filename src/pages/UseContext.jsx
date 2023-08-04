84abdb5b5b26045028dbe037753a960721421f40
Elar Saks <elarsaks@gmail.com>
2023-08-05 01:39:58 +0300

    Fix useContext.

--- src/pages/UseContext.jsx
+++ src/pages/UseContext.jsx
@@ -1,7 +1,8 @@
 import ClassContextComponent from "../components/ClassContextComponent";
 import FunctionContextComponent from "../components/FunctionContextComponent";
 import Button from "../components/Button";
-import React, { useState } from "react";
+import info from "../styles/Info.module.css";
+import React, { useEffect, useState, useRef } from "react";
 import util from "../styles/Util.module.css";
 
 // Create a new context instance
@@ -19,6 +20,16 @@ export default () => {
   return (
     <>
       <h1 className={util["header-1"]}>- useContext -</h1>
+      <h2 className={`${info["info"]} ${info["border-bottom"]}`}>
+        <span>useContext</span> is a React hook that accepts a context object
+        (the value returned from React.createContext) and returns the current{" "}
+        <span> context value </span>
+        for that context. The current context value is determined by the{" "}
+        <span>nearest </span> matching Provider up the tree from where the hook
+        is called, allowing you to easily access shared data without passing
+        <span> props </span> down through every level of the{" "}
+        <span> component </span> tree.
+      </h2>
 
       {/* ThemeContext provider that provides the value of 'darkTheme' to all children */}
       <ThemeContext.Provider value={darkTheme}>
@@ -35,6 +46,18 @@ export default () => {
         {/* Class component consuming the context value using the 'contextType' property */}
         <ClassContextComponent />
       </ThemeContext.Provider>
+
+      <h2 className={`${info["info"]} ${info["border-top"]}`}>
+        When the <span>Toggle Theme</span> button is clicked, the theme state is
+        updated and passed via <span>context</span> to both functional and
+        class-based theme components, which use the <span>context</span> value
+        to render.
+        <br></br> <br></br>
+        Functional components use the <span>useContext hook </span>
+        to consume <span>context</span>, while class components use the
+        <span> static contextType</span> property to consume{" "}
+        <span>context</span>.
+      </h2>
     </>
   );
 };
