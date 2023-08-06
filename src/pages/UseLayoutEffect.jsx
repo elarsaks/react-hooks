6fc1266c36fb3629981826e553ed4bf4a4530c72
Elar Saks <elarsaks@gmail.com>
2023-08-06 19:22:45 +0300

    Finnish useLayoutEffect.

--- src/pages/UseLayoutEffect.jsx
+++ src/pages/UseLayoutEffect.jsx
@@ -1,9 +1,34 @@
 // Importing the necessary dependencies
-import React from "react";
+import React, { useState, useLayoutEffect } from "react";
 import info from "../styles/Info.module.css";
 import util from "../styles/Util.module.css";
+import Button from "../components/Button";
 
 export default () => {
+  const [count, setCount] = useState(() => 0);
+  const handleIncrement = () => {
+    setCount((previousCount) => previousCount + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount((previousCount) => previousCount - 1);
+  };
+
+  // The useLayoutEffect hook is similar to the useEffect hook.
+  // The difference is that it fires synchronously after all DOM mutations.
+  // This hook is useful when you need to read or write the DOM before the browser paints.
+  // It's recommended to use useEffect instead of useLayoutEffect.
+  // This is because useLayoutEffect can block visual updates, making your app feel sluggish.
+  // However, if you need to make DOM measurements or trigger a synchronous re-render,
+  // useLayoutEffect is the right choice.
+  useLayoutEffect(() => {
+    if (count === 0) {
+      document.title = "React Hooks";
+    } else {
+      document.title = `Count: ${count}`;
+    }
+  }, [count]);
+
   return (
     <>
       <h1 className={util["header-1"]}>- useLayoutEffect -</h1>
@@ -13,8 +38,17 @@ export default () => {
         synchronously after DOM mutations, ensuring that the UI is updated
         before the browser paints.
       </h2>
-      ...
-      <h2 className={`${info["info"]} ${info["border-top"]}`}></h2>
+
+      <Button text="-" handleClick={handleDecrement} />
+      <span className={util["counter"]}>{count}</span>
+      <Button text="+" handleClick={handleIncrement} />
+
+      <h2 className={`${info["info"]} ${info["border-top"]}`}>
+        In this demonstration, we've leveraged the <span>useLayoutEffect</span>{" "}
+        hook to update the document title. The title is updated synchronously
+        after the DOM is updated, ensuring that the UI is updated before the
+        browser paints.
+      </h2>
     </>
   );
 };
